a600ddba976ca51d6c6cf2b9d03d951bd82f0cfd
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-09-15 17:10:23 +0530

    chore(deps): revert the compose plugin to 2.4.10 for codeql

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "9.4.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("com.google.devtools.ksp") version "2.3.12" apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
     id("com.google.firebase.crashlytics") version "3.0.8" apply false
